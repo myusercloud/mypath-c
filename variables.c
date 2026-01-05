2ac38744d8a2b6f416f9163fa8dbd881155dd060
Myusercloud | Harrizon <lucasharrizon@gmail.com>
2026-01-05 10:20:29 +0300

    learn variables

---
+++ variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("Let us learn about variables in C!\n");
+
+    int age = 21;
+    double height = 5.9;
+    char name[] = "Harry";
+
+    printf("My name is %s\n", name);
+    printf("I am %d years old and I'm %.1f feet tall\n", age, height);
+
+    return 0;
+}
