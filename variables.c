841791e80b575aeca526faa8113b325ec7c543d1
Myusercloud | Harrizon <lucasharrizon@gmail.com>
2026-01-06 21:54:00 +0300

    add explanation about variables in C

--- variables.c
+++ variables.c
@@ -9,6 +9,7 @@ int main() {
 
     printf("My name is %s\n", name);
     printf("I am %d years old and I'm %.1f feet tall\n", age, height);
+    printf("Variables are used to store data in a program.\n");
 
     return 0;
 }
